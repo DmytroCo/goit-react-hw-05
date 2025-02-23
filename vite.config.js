86e434d15ffedec813f05353f1acdd8d76726380
Dmytro Popsha <popsha.dima@gmail.com>
2025-02-23 20:54:21 +0200

    createComponents

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.themoviedb.org',
+        changeOrigin: true,
+        secure: false,  
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
